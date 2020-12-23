45b89b526e0acd4d20c8955f3d649bfce5129687
Sergio Linero <SLineroDev@gmail.com>
2020-12-23 21:35:40 +0100

    Extracted Router to separate component

--- React/src/app.tsx
+++ React/src/app.tsx
@@ -4,23 +4,12 @@ import { LoginPage } from './login';
 import { ListPage } from './list';
 import { DetailPage } from './detail';
 import { MyContextProvider } from './context';
+import { MyRouter } from './myRouter';
 
 export const App = () => {
 	return (
 		<MyContextProvider>
-			<Router>
-				<Switch>
-					<Route exact path="/">
-						<LoginPage />
-					</Route>
-					<Route path="/list">
-						<ListPage />
-					</Route>
-					<Route path="/detail/:id">
-						<DetailPage />
-					</Route>
-				</Switch>
-			</Router>
+			<MyRouter />
 		</MyContextProvider>
 	);
 };
